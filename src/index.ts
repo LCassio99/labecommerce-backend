7b8020e1bad17bb8c18a882861ca243ef7c664bf
Luciano Brito <lucianocassio66@gmail.com>
2023-12-05 20:30:35 -0300

    apis & express

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,94 @@
 
 //console.log('O aplicativo foi iniciado')
 
+import express, { Request, Response } from 'express'
+import cors from 'cors'
 import { createUser, getAllUsers, products, users, createProduct, getAllProducts, searchProductsByName } from './database';
+import { TProducts, TUsers } from './types'
 
 //console.log(users, products);
 
-const createUserMessage = createUser('u003', 'Astrodev', 'astrodev@email.com', 'astrodev99')
-console.log(createUserMessage)
+const app = express()
 
-const allUsers = getAllUsers()
-console.log('Lista de usuários: ', getAllUsers())
+app.use(express.json())
+app.use(cors())
 
-const createProductMessage = createProduct('prod003', 'SSD gamer', 349.99, 'Acelere seu sistema com velocidades incríveis de leitura e gravação.', 'https://images.unsplash.com/photo')
-console.log(createProductMessage)
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003");
+})
 
-const allProducts = getAllProducts()
-console.log('Lsita de produtos', allProducts)
+app.get('/ping', (req: Request, res: Response) => {
+    res.send('Arroz!')
+})
 
-const searchTerm = 'gamer'
-const productsByName = searchProductsByName(searchTerm)
+//getAllUsers
 
-console.log(`Produtos com o termo ${searchTerm}:`, productsByName)
+app.get('/users', (req: Request, res: Response)=> {
+    res.send(users)
+})
+
+//getAllProducts
+
+app.get('/products', (req: Request, res: Response) => {
+    const nameToFind = req.query.name as string
+    if(nameToFind){
+        const result: TProducts[] = products.filter((product) => {
+            return product.name.toLowerCase().includes(nameToFind.toLowerCase())
+        })
+        res.send(result)
+    }
+    res.send(products)
+})
+
+//findProductByName
+
+app.get('/product', (req: Request, res: Response) => {
+    const nameToFind = req.query.name as string
+    const result: TProducts[] = products.filter((product) => {
+        return product.name.toLowerCase().includes(nameToFind.toLowerCase())
+    })
+    res.send(result)
+    if(result === undefined){
+        res.send(products)
+    }
+})
+
+//createUser
+
+app.post('/users', (req: Request, res: Response) => {
+    const id = req.body.id as string
+  const name = req.body.name as string
+  const email = req.body.email as string
+  const password = req.body.password as string
+  
+  const newUser: TUsers = {
+    id,
+    name,
+    email,
+    password,
+    createdAt: new Date().toISOString(),
+  }
+  users.push(newUser);
+  res.status(201).send('Cadastro realizado com sucesso!')
+})
+
+//createProduct
+
+app.post('/products', (req: Request, res: Response) => {
+    const id = req.body.id as string
+    const name = req.body.name as string
+    const price = req.body.price as number
+    const description = req.body.description as string
+    const imageUrl = req.body.imageUrl as string
+    
+    const newProduct: TProducts = {
+        id,
+        name,
+        price,
+        description,
+        imageUrl
+    }
+
+    products.push(newProduct)
+    res.status(201).send('Produto cadastrado com sucesso!')
+})
